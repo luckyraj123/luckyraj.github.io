7722649cb650c70023057573ebe232895ddb0648
luckyraj <divyarajsinh2109@gmail.com>
2021-11-10 19:07:49 +0530

    1

---
+++ food/script.js
@@ -0,0 +1,30 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = ()=>{
+
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 60){
+        Document.querySelector('#scroll-top').classList.add('active'); 
+    }else{
+        Document.querySelector('#scroll-top').classList.remove('active');
+    }
+}
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out'); 
+
+}
+function fadeOut(){
+    setInterval(loader, 5000);
+}
+
+window.onload = fadeOut();
