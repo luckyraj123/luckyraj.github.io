7ca6a491363b22a5643415d1e04b0448638687d7
luckyraj <divyarajsinh2109@gmail.com>
2022-07-05 16:56:50 +0530

    aa

--- ChatApp/script/script.js
+++ ChatApp/script/script.js
@@ -1,4 +1,20 @@
 function StartChat(id) {
     document.getElementById('chatpanel').removeAttribute('style');
     document.getElementById('divstart').setAttribute('style','display');
+
+
+    hideChatApp();
+}
+////////////////////////////////////////////////
+
+////show chat list
+
+function showChatList(){
+    document.getElementById('side-1').classList.remove('d-none','d-md-block');
+    document.getElementById('side-2').classList.add('d-none');
+}
+
+function hideChatApp(){
+    document.getElementById('side-1').classList.add('d-none','d-md-block');
+    document.getElementById('side-2').classList.remove('d-none');
 }
