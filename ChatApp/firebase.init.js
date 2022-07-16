acbef5e6dc67c062603157a60c782ae7f5d8b631
luckyraj <divyarajsinh2109@gmail.com>
2022-07-16 15:12:09 +0530

    2

---
+++ ChatApp/firebase.init.js
@@ -0,0 +1,11 @@
+    var firebaseConfig = {
+    apiKey: "AIzaSyBCxk5p6P7TzT2B3sbEjxbFMIIpPNS12iQ",
+    authDomain: "chatapp-ebbae.firebaseapp.com",
+    projectId: "chatapp-ebbae",
+    storageBucket: "chatapp-ebbae.appspot.com",
+    messagingSenderId: "709981543671",
+    appId: "1:709981543671:web:a3e592f5930efb9c2307f2"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- ChatApp/script/script.js
+++ ChatApp/script/script.js
@@ -45,5 +45,42 @@ function SendMessage() {
     document.getElementById('txtMessage').value ='';
     document.getElementById('txtMessage').focus();
 
-    document.getElementById('messages').scrollTo(0,document.getElementById('messages').clientHeight )
+    document.getElementById('messages').scrollTo(0, document.getElementById('messages').clientHeight)
 }
+
+
+/////////////////////////////////////
+// FIREBASE CODE 
+
+// function singin(){
+//     var provider = new firebase.auth.GoogleAuthProvider();
+//     firebase.auth().signInWithPopup(provider);
+// }
+var provider  = firebase.auth.GoogleAuthProvider();
+
+function googleSignin() {
+   firebase.auth()
+   
+   .signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+		
+      console.log(token)
+      console.log(user)
+   }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+		
+      console.log(error.code)
+      console.log(error.message)
+   });
+}
+function googleSignout() {
+    firebase.auth().signOut()
+     
+    .then(function() {
+       console.log('Signout Succesfull')
+    }, function(error) {
+       console.log('Signout Failed')  
+    });
+ }
